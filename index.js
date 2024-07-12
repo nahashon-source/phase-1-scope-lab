3c9d82d1a9950cb7e1084ebb8df3fa97a7339f77
nahashon <nashonmwendwa0@gmail.com>
2024-07-12 22:24:08 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  window.bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'alice';
+
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = 'charlie';
+  } catch (error) {
+    console.log("Can't change a constant!");
+    throw error; // rethrow the error
+  }
+}
